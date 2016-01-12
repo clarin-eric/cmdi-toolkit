92e353728344024906e9f20a77abc7ff6fed7b05
meaton <seaton@hum.ku.dk>
2016-01-12 14:41:23 +0000

    M    pom.xml   * updated repository url
    M    src/main/resources/toolkit/xsd/cmd-component.xsd
      * Removed Schematron rule for Attribute name values not longer required due to 1.2 namespaces changes
    
    M    src/test/java/eu/clarin/cmd/toolkit/TestCMDToolkit.java
      * Added unit test for CMDI profile schema containing 'ref' named Attributes

--- toolkit/src/test/java/eu/clarin/cmd/toolkit/TestCMDToolkit.java
+++ toolkit/src/test/java/eu/clarin/cmd/toolkit/TestCMDToolkit.java
@@ -46,8 +46,8 @@ public class TestCMDToolkit {
     XsltTransformer upgradeCMDSpec = null;
     XsltTransformer upgradeCMDRec = null;
     XsltTransformer transformCMDSpecInXSD = null;
-    SchemAnon validateCMDSpec = null; 
-    SchemAnon validateCMDEnvelop = null; 
+    SchemAnon validateCMDSpec = null;
+    SchemAnon validateCMDEnvelop = null;
 
     @Before
     public void setUp() {
@@ -55,8 +55,8 @@ public class TestCMDToolkit {
             upgradeCMDSpec = SaxonUtils.buildTransformer(CMDToolkit.class.getResource("/toolkit/upgrade/cmd-component-1_1-to-1_2.xsl")).load();
             upgradeCMDRec = SaxonUtils.buildTransformer(CMDToolkit.class.getResource("/toolkit/upgrade/cmd-record-1_1-to-1_2.xsl")).load();
             transformCMDSpecInXSD = SaxonUtils.buildTransformer(CMDToolkit.class.getResource("/toolkit/xslt/comp2schema.xsl")).load();
-            validateCMDSpec = new SchemAnon(CMDToolkit.class.getResource("/toolkit/xsd/cmd-component.xsd").toURI().toURL()); 
-            validateCMDEnvelop = new SchemAnon(CMDToolkit.class.getResource("/toolkit/xsd/cmd-envelop.xsd").toURI().toURL()); 
+            validateCMDSpec = new SchemAnon(CMDToolkit.class.getResource("/toolkit/xsd/cmd-component.xsd").toURI().toURL());
+            validateCMDEnvelop = new SchemAnon(CMDToolkit.class.getResource("/toolkit/xsd/cmd-envelop.xsd").toURI().toURL());
         } catch(Exception e) {
             System.err.println("!ERR: couldn't setup the testing environment!");
             System.err.println(""+e);
@@ -67,7 +67,7 @@ public class TestCMDToolkit {
     @After
     public void tearDown() {
     }
-    
+
     protected Document transform(XsltTransformer trans,Source src) throws Exception {
         try {
             DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
@@ -86,7 +86,7 @@ public class TestCMDToolkit {
             throw e;
         }
     }
-    
+
     protected boolean xpath(Document doc,String xpath) throws Exception {
         XPathCompiler xpc   = SaxonUtils.getProcessor().newXPathCompiler();
         xpc.declareNamespace("cmd","http://www.clarin.eu/cmd/1");
@@ -95,21 +95,21 @@ public class TestCMDToolkit {
         xps.setContextItem(SaxonUtils.getProcessor().newDocumentBuilder().wrap(doc));
         return xps.effectiveBooleanValue();
     }
-    
+
     protected void printMessages(SchemAnon anon) throws Exception {
         for (Message msg : anon.getMessages()) {
             System.out.println("" + (msg.isError() ? "ERROR" : "WARNING") + (msg.getLocation() != null ? " at " + msg.getLocation() : ""));
             System.out.println("  " + msg.getText());
         }
     }
-    
+
     protected int countErrors(SchemAnon anon) throws Exception {
         int cnt = 0;
         for (Message msg : anon.getMessages())
             cnt += (msg.isError()?1:0);
         return cnt;
     }
-    
+
     protected Document upgradeCMDSpec(String spec) throws Exception {
         System.out.println("Upgrade CMD spec["+spec+"]");
         return transform(upgradeCMDSpec,new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDToolkit.class.getResource(spec).toURI())));
@@ -119,16 +119,16 @@ public class TestCMDToolkit {
         System.out.println("Upgrade CMD spec["+rec+"]");
         return transform(upgradeCMDRec,new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDToolkit.class.getResource(rec).toURI())));
     }
-    
-    protected Document transformCMDSpecInXSD(String spec,Source src) throws Exception { 
+
+    protected Document transformCMDSpecInXSD(String spec,Source src) throws Exception {
         System.out.println("Transform CMD spec["+spec+"] into XSD");
         return transform(transformCMDSpecInXSD,src);
     }
-    
+
     protected Document transformCMDSpecInXSD(String spec) throws Exception {
         return transformCMDSpecInXSD(spec,new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDToolkit.class.getResource(spec).toURI())));
     }
-    
+
     protected boolean validateCMDSpec(String spec,Source src) throws Exception {
         System.out.println("Validate CMD spec["+spec+"]");
         boolean res = validateCMDSpec.validate(src);
@@ -136,11 +136,11 @@ public class TestCMDToolkit {
         printMessages(validateCMDSpec);
         return res;
     }
-    
+
     protected boolean validateCMDSpec(String spec) throws Exception {
         return validateCMDSpec(spec,new StreamSource(new java.io.File(TestCMDToolkit.class.getResource(spec).toURI())));
     }
-    
+
     protected boolean validateCMDRecord(String spec,SchemAnon anon,String rec,Source src) throws Exception {
         System.out.println("Validate CMD record["+rec+"] against spec["+spec+"]");
         boolean res = anon.validate(src);
@@ -148,7 +148,7 @@ public class TestCMDToolkit {
         printMessages(anon);
         return res;
     }
-    
+
     protected boolean validateCMDEnvelop(String rec,Source src) throws Exception {
         System.out.println("Validate envelop CMD rec["+rec+"]");
         boolean res = validateCMDEnvelop.validate(src);
@@ -156,38 +156,38 @@ public class TestCMDToolkit {
         printMessages(validateCMDEnvelop);
         return res;
     }
-    
+
     protected boolean validateCMDEnvelop(String rec) throws Exception {
         return validateCMDEnvelop(rec,new StreamSource(new java.io.File(TestCMDToolkit.class.getResource(rec).toURI())));
     }
-    
+
     @Test
     public void testAdelheid() throws Exception {
         String profile = "/toolkit/Adelheid/profiles/clarin.eu:cr1:p_1311927752306.xml";
         String record  = "/toolkit/Adelheid/records/Adelheid.cmdi";
-        
+
         // upgrade the profile from 1.1 to 1.2
         Document upgradedProfile = upgradeCMDSpec(profile);
-        
+
         // validate the 1.2 profile
         boolean validProfile = validateCMDSpec(profile+" (upgraded)",new DOMSource(upgradedProfile));
-        
+
         // assertions
         // the upgraded profile should be a valid CMDI 1.2 profile
         assertTrue(validProfile);
         // so there should be no errors
         assertEquals(0, countErrors(validateCMDSpec));
-        
+
         // transform the 1.2 profile into a XSD
         Document profileSchema = transformCMDSpecInXSD(profile+" (upgraded)",new DOMSource(upgradedProfile));
         SchemAnon profileAnon = new SchemAnon(new DOMSource(profileSchema));
-        
+
         // upgrade the record from 1.1 to 1.2
         Document upgradedRecord = upgradeCMDRecord(record);
-        
+
         // validate the 1.2 record
         boolean validRecord = validateCMDRecord(profile+" (upgraded)",profileAnon,record+" (upgraded)",new DOMSource(upgradedRecord));
-        
+
         // assertions
         // the upgraded record should be a valid CMDI 1.2 record
         assertTrue(validRecord);
@@ -199,25 +199,60 @@ public class TestCMDToolkit {
     public void testAdelheid2() throws Exception {
         String profile = "/toolkit/Adelheid/profiles/clarin.eu:cr1:p_1311927752306_1_2.xml";
         String record  = "/toolkit/Adelheid/records/Adelheid_1_2-invalid.cmdi";
-        
+
         // validate the 1.2 profile
         boolean validProfile = validateCMDSpec(profile,new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDToolkit.class.getResource(profile).toURI())));
-        
+
         // assertions
         // the upgraded profile should be a valid CMDI 1.2 profile
         assertTrue(validProfile);
         // so there should be no errors
         assertEquals(0, countErrors(validateCMDSpec));
-        
+
         Document profileSchema = transformCMDSpecInXSD(profile,new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDToolkit.class.getResource(profile).toURI())));
         SchemAnon profileAnon = new SchemAnon(new DOMSource(profileSchema));
-        
+
         // validate the 1.2 record
         boolean validRecord = validateCMDRecord(profile,profileAnon,record,new javax.xml.transform.stream.StreamSource(new java.io.File(TestCMDToolkit.class.getResource(record).toURI())));
-        
+
         // assertions
         // the record should be a invalid as it misses a the required CoreVersion attribute
         assertFalse(validRecord);
+    }
+
+    @Test
+    public void testSundhed() throws Exception {
+        String profile = "/toolkit/TEI/profiles/clarin.eu:cr1:p_1380106710826.xml";
+        String record  = "/toolkit/TEI/records/sundhed_dsn.teiHeader.ref.xml";
+
+        // upgrade the profile from 1.1 to 1.2
+        Document upgradedProfile = upgradeCMDSpec(profile);
+
+        // validate the 1.2 profile
+        boolean validProfile = validateCMDSpec(profile+" (upgraded)",new DOMSource(upgradedProfile));
+
+        // assertions
+        // the upgraded profile should be a valid CMDI 1.2 profile and 'ref' named Attributes should validate
+        assertTrue(validProfile);
+        // so there should be no errors
+        assertEquals(0, countErrors(validateCMDSpec));
+
+        // transform the 1.2 profile into a XSD
+        Document profileSchema = transformCMDSpecInXSD(profile+" (upgraded)",new DOMSource(upgradedProfile));
+        SchemAnon profileAnon = new SchemAnon(new DOMSource(profileSchema));
+
+        // upgrade the record from 1.1 to 1.2
+        Document upgradedRecord = upgradeCMDRecord(record);
+
+        // validate the 1.2 record
+        boolean validRecord = validateCMDRecord(profile+" (upgraded)", profileAnon, record+" (upgraded)", new DOMSource(upgradedRecord));
+
+        // assertions
+        // the upgraded record should be a valid CMDI 1.2 record
+        assertTrue(validRecord);
+
+        // so there should be no errors
+        assertEquals(0, countErrors(profileAnon));
     }
 
     @Test
@@ -226,7 +261,7 @@ public class TestCMDToolkit {
 
         // upgrade the record from 1.1 to 1.2
         Document upgradedRecord = upgradeCMDRecord(record);
-        
+
         // assertions
         // the @ref attributes on the elements should stay as they are
         assertTrue(xpath(upgradedRecord,"//*:author/*:name/@ref"));
@@ -246,26 +281,26 @@ public class TestCMDToolkit {
     public void testOLAC() throws Exception {
         String profile = "/toolkit/OLAC/profiles/OLAC-DcmiTerms.xml";
         String record  = "/toolkit/OLAC/records/org_rosettaproject-record.xml";
-        
+
         // upgrade the profile from 1.1 to 1.2
         Document upgradedProfile = upgradeCMDSpec(profile);
-        
+
         // validate the 1.2 profile
         boolean validProfile = validateCMDSpec(profile+" (upgraded)",new DOMSource(upgradedProfile));
-        
+
         // assertions
         // the upgraded profile should be a valid CMDI 1.2 profile
         assertTrue(validProfile);
         // so there should be no errors
         assertEquals(0, countErrors(validateCMDSpec));
-        
+
         // transform the 1.2 profile into a XSD
         Document profileSchema = transformCMDSpecInXSD(profile+" (upgraded)",new DOMSource(upgradedProfile));
         SchemAnon profileAnon = new SchemAnon(new DOMSource(profileSchema));
-        
+
         // upgrade the record from 1.1 to 1.2
         Document upgradedRecord = upgradeCMDRecord(record);
-        
+
         // validate the 1.2 record
         boolean validRecord = validateCMDRecord(profile+" (upgraded)",profileAnon,record+" (upgraded)",new DOMSource(upgradedRecord));
 
@@ -283,4 +318,6 @@ public class TestCMDToolkit {
         // assertions
         assertFalse(validProfile);
     }
+
+
 }

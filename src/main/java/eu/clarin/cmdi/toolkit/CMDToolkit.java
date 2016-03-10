311193cefb7ea2f5693a1eb3f1b5427afc5677fb
Twan Goosen <twan.goosen@mpi.nl>
2016-03-10 14:40:54 +0100

    Added (some) constants with resource locations to CMDToolkit class

--- src/main/java/eu/clarin/cmdi/toolkit/CMDToolkit.java
+++ src/main/java/eu/clarin/cmdi/toolkit/CMDToolkit.java
@@ -5,4 +5,14 @@
 package eu.clarin.cmdi.toolkit;
 
 public class CMDToolkit {
+    
+    public static final String XSLT_RECORD_UPGRADE = "/toolkit/upgrade/cmd-record-1_1-to-1_2.xsl";
+    public static final String XSLT_COMPONENT_UPGRADE = "/toolkit/upgrade/cmd-component-1_1-to-1_2.xsl";
+    //toolkit/downgrade/cmd-component-1_2-to-1_1.xsl
+    //toolkit/sch/cmd-component-best-practices.sch
+    //toolkit/sch/cmd-record-best-practices.sch
+    //toolkit/xsd/cmd-component.xsd
+    //toolkit/xsd/cmd-envelop.xsd
+    //toolkit/xslt/clavas2enum.xsl
+    //toolkit/xslt/comp2schema.xsl
 }

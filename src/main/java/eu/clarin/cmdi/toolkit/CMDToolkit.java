e151f1d1d2522f58c6c94c2cb2085fddd4f91ff5
Twan Goosen <twan.goosen@mpi.nl>
2016-03-16 11:57:59 +0100

    Constant for component schema resource

--- src/main/java/eu/clarin/cmdi/toolkit/CMDToolkit.java
+++ src/main/java/eu/clarin/cmdi/toolkit/CMDToolkit.java
@@ -8,10 +8,10 @@ public class CMDToolkit {
     
     public static final String XSLT_RECORD_UPGRADE = "/toolkit/upgrade/cmd-record-1_1-to-1_2.xsl";
     public static final String XSLT_COMPONENT_UPGRADE = "/toolkit/upgrade/cmd-component-1_1-to-1_2.xsl";
+    public static final String COMPONENT_SCHEMA = "/toolkit/xsd/cmd-component.xsd";
     //toolkit/downgrade/cmd-component-1_2-to-1_1.xsl
     //toolkit/sch/cmd-component-best-practices.sch
     //toolkit/sch/cmd-record-best-practices.sch
-    //toolkit/xsd/cmd-component.xsd
     //toolkit/xsd/cmd-envelop.xsd
     //toolkit/xslt/clavas2enum.xsl
     //toolkit/xslt/comp2schema.xsl

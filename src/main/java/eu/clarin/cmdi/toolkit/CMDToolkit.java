bbd4685f2888713f0ff094e7857321009bfda526
Twan Goosen <twan.goosen@mpi.nl>
2016-03-31 11:03:04 +0200

    Added constants for the component schema's schematron phases

--- src/main/java/eu/clarin/cmdi/toolkit/CMDToolkit.java
+++ src/main/java/eu/clarin/cmdi/toolkit/CMDToolkit.java
@@ -16,4 +16,6 @@ public class CMDToolkit {
     //toolkit/xslt/clavas2enum.xsl
     //toolkit/xslt/comp2schema.xsl
     public static final String XSLT_PARAM_COMP2SCHEMA_TOOL_KITLOCATION = "cmd-toolkit";
+    public static final String SCHEMATRON_PHASE_CMD_COMPONENT_PRE_REGISTRATION = "preRegistration";
+    public static final String SCHEMATRON_PHASE_CMD_COMPONENT_POST_REGISTRATION = "postRegistration";
 }
